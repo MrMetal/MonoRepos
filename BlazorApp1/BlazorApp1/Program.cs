efc32e2426d9f302c132792e9102c84794ec0064
Thiago Fortini <tiago.fortini784@gmail.com>
2024-07-03 16:32:04 -0300

    add mudblazor

--- BlazorApp1/BlazorApp1/Program.cs
+++ BlazorApp1/BlazorApp1/Program.cs
@@ -1,11 +1,13 @@
 using BlazorApp1.Client.Pages;
 using BlazorApp1.Components;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveWebAssemblyComponents();
+builder.Services.AddRazorComponents().AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddMudServices();
 
 var app = builder.Build();
 
